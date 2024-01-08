d09cd9e41814ae9a9a3656322992af00edb95824
danial <info@danialgermany.insta>
2024-01-08 23:16:00 +0100

    S06

---
+++ S06/src/com/mysite/customer/ApplicationRunner.java
@@ -0,0 +1,12 @@
+package com.mysite.customer;
+
+import com.mysite.customer.view.ConsoleUI;
+
+
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        try (ConsoleUI consoleUI = new ConsoleUI()){
+            consoleUI.startMenu();
+        }
+    }
+}
