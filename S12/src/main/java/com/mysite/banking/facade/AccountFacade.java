c30bfc0ebf2fc0e12ad2bb09230fe88f68400381
danial <info@danialgermany.insta>
2024-02-18 23:45:43 +0100

    S12

---
+++ S12/src/main/java/com/mysite/banking/facade/AccountFacade.java
@@ -0,0 +1,36 @@
+package com.mysite.banking.facade;
+
+import com.mysite.banking.dto.AccountDto;
+import com.mysite.banking.model.FileType;
+import com.mysite.banking.service.exception.AccountNotFindException;
+import com.mysite.banking.service.exception.EmptyAccountException;
+import com.mysite.banking.service.exception.FileException;
+import com.mysite.banking.service.exception.ValidationException;
+
+import java.util.List;
+
+public interface AccountFacade {
+    void deleteAccountById(Integer id) throws AccountNotFindException;
+    List<AccountDto> getActiveAccounts() throws EmptyAccountException;
+    List<AccountDto> getDeletedAccounts() throws EmptyAccountException;
+    AccountDto getAccountById(Integer id) throws AccountNotFindException;
+    void addAccount(AccountDto accountDto) throws ValidationException;
+    void updateAccount(AccountDto accountDto) throws ValidationException, AccountNotFindException;
+    void saveData(String name, FileType type) throws FileException;
+
+    void loadData(String name, FileType fileType) throws FileException;
+
+    void initData();
+
+    void saveOnExit();
+
+    void addData(String name) throws FileException;
+
+    List<AccountDto> searchAccountByCustomerName(String name);
+
+    void deposit(int accountId, Double amount) throws AccountNotFindException;
+
+    void withdraw(int accountId, Double amount)  throws AccountNotFindException, ValidationException;
+
+    void transfer(int fromAccountId, int toAccountId, Double amount) throws AccountNotFindException, ValidationException;
+}
