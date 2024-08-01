73f64c535c24554538f2a73da66ff7311d8d23d6
danial <info@danialgermany.insta>
2024-08-01 12:12:30 +0200

    S06

---
+++ S06/src/com/mysite/customer/util/ScannerWrapper.java
@@ -0,0 +1,31 @@
+package com.mysite.customer.util;
+
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class ScannerWrapper implements AutoCloseable {
+
+    private static final ScannerWrapper INSTANCE;
+    private final Scanner scanner;
+    public static ScannerWrapper getInstance(){
+        return INSTANCE;
+    }
+
+    static {
+        INSTANCE = new ScannerWrapper();
+    }
+
+    private ScannerWrapper() {
+        scanner = new Scanner(System.in);
+    }
+
+    public <T> T getUserInput(String message, Function<String, T> converter) {
+        System.out.println(message);
+        return converter.apply(scanner.nextLine());
+    }
+
+    @Override
+    public void close() {
+        scanner.close();
+    }
+}
