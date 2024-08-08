281e9a7592910df8997b2787e4b15c33d3bd418c
danial <info@danialgermany.insta>
2024-08-08 15:35:13 +0200

    S08

---
+++ S08/src/com/mysite/customer/service/exception/CustomerBaseException.java
@@ -0,0 +1,11 @@
+package com.mysite.customer.service.exception;
+
+public class CustomerBaseException extends Exception{
+    public CustomerBaseException(String message){
+        super(message);
+    }
+
+    public CustomerBaseException(){
+        super();
+    }
+}
