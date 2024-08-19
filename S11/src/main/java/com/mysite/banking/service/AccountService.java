e1b209bc83559d5a6f798d28dfc996f7ee8eed27
danial <info@danialgermany.insta>
2024-08-19 18:49:31 +0200

    S11

---
+++ S11/src/main/java/com/mysite/banking/service/AccountService.java
@@ -0,0 +1,27 @@
+package com.mysite.banking.service;
+
+import com.mysite.banking.model.Account;
+import com.mysite.banking.model.FileType;
+import com.mysite.banking.service.exception.*;
+
+import java.util.List;
+
+public interface AccountService {
+    void deleteAccountById(Integer id) throws AccountNotFindException;
+    List<Account> getActiveAccounts() throws EmptyAccountException;
+    List<Account> getDeletedAccounts() throws EmptyAccountException;
+    Account getAccountById(Integer id) throws AccountNotFindException;
+    void addAccount(Account account);
+    void saveData(String name, FileType type) throws FileException;
+    void loadData(String name, FileType fileType) throws FileException;
+    void initData();
+    void saveOnExit();
+    void addData(String name) throws FileException;
+    List<Account> getAccountByCustomerId(Integer id);
+
+    void deposit(int accountId, Double amount) throws AccountNotFindException;
+
+    void withdraw(int accountId, Double amount) throws AccountNotFindException, ValidationException;
+
+    void transfer(int fromAccountId, int toAccountId, Double amount) throws AccountNotFindException, ValidationException;
+}
