340900042d97b1e95751a6735eb522666af91211
danial <info@danialgermany.insta>
2024-12-31 08:12:10 +0100

    S08

---
+++ S08/src/com/mysite/customer/service/validation/Validation.java
@@ -0,0 +1,8 @@
+package com.mysite.customer.service.validation;
+
+import com.mysite.customer.service.exception.ValidationException;
+
+@FunctionalInterface
+public interface Validation <T> {
+    void validate(T t) throws ValidationException;
+}
