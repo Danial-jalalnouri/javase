8aa42d2ebe60ef8f726ad5bc1e20af7286dba682
danial <info@danialgermany.insta>
2025-02-23 07:57:20 +0100

    S16

---
+++ S16/src/main/java/com/mysite/banking/service/exception/UpdateException.java
@@ -0,0 +1,11 @@
+package com.mysite.banking.service.exception;
+
+public class UpdateException extends RuntimeException{
+    public UpdateException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public UpdateException(){
+        super();
+    }
+}
